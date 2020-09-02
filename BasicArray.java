05c3cd7220a1dbb5d4359559ae94605f7a825e39
Serra Saracoglu <62646936+SerraSrcgl@users.noreply.github.com>
2020-09-02 23:22:08 +0200

    Add files via upload

---
+++ BasicArray.java
@@ -0,0 +1,13 @@
+package Arrays;
+
+public class BasicArray {
+    public static void main(String [] args){
+        int array[][]=new int[10][10];
+        for(int i=0;i< 10;i++){
+            for(int j=0;j<10;j++){
+                System.out.print("*");
+
+        }System.out.println();
+
+    }
+}}
